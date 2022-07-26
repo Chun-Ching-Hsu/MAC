ce8ded9e94076d871a7350a760a7547d5fec3c6c
Chun-Ching-Hsu <68492756+Chun-Ching-Hsu@users.noreply.github.com>
2022-07-26 23:24:38 +0800

    學姊改過的

--- Pointer.v
+++ Pointer.v
@@ -1,13 +1,18 @@
 module Pointer
     #(parameter BufferWidth = 2)
-    (EN,Pointer,clk);
+    (clk, reset, EN, Pointer);
     input clk;
+    input reset;
     input EN;
 
-    output [BufferWidth-1:0] Pointer;
+    output reg [BufferWidth-1:0] Pointer;
 
     always@(posedge clk)begin
-        if(EN)begin
+        // 學姊增加了reset 功能 20220726
+        if(reset) begin
+            Pointer <= 0;
+        end
+        else if(EN) begin
 	        Pointer <= Pointer+1;
         end
         else begin
