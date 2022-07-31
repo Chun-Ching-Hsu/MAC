f906f8e0c0408ac82673ff5c968a759fdaa7d414
Chun-Ching-Hsu <68492756+Chun-Ching-Hsu@users.noreply.github.com>
2022-07-31 20:18:31 +0800

    new

--- Buffer.v
+++ Buffer.v
@@ -2,18 +2,28 @@ module Buffer
     #(  parameter DataWidth = 8,
         parameter BufferSize = 4
         parameter BufferWidth = 2 )
-    (   clk,EN,DataIn,DataOut1,DataOut2,W_Addr,R_Addr1.R_Addr2 );
+    ( clk,reset,EN,DataIn,DataOut1,DataOut2,W_Addr,R_Addr1.R_Addr2 );
     input clk;
     input EN;
+    // 新增 reset 
+    input reset;
     input [BufferWidth-1:0] W_Addr,R_Addr1,R_Addr2;
     input [DataWidth-1:0] DataIn;
-    #測試是否ok
+    //測試是否ok
     reg [DataWidth-1:0] Buffer [0:BufferSize-1];
 
     output [DataWidth-1:0] DataOut1,DataOut2;
+    
+    // 32位元有號數 多用在for
+    integer index;
 
     always @(posedge clk)begin
-        if(EN)begin
+        if(reset)begin
+            for (index = 0; index < BufferSize; index = index + 1)begin // resetBuf
+                Buffer[index] <= 0;
+            end
+        end
+        else if(EN)begin
             Buffer[W_Addr] <= DataIn;
         end
     end
